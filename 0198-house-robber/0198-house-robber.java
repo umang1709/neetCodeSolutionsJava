95ce1feb93a4bb6fae1f1af3115e9e24e050387e
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-01-19 08:44:38 +0530

    Time: 0 ms (100.00%), Space: 41 MB (21.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -6,11 +6,14 @@ class Solution {
         int len = nums.length;
         if(len<2)return nums[0];
         int[] dp = new int[len];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        int r1 = nums[0];
+        int r2 = Math.max(nums[0],nums[1]);
         for(int i=2;i<len;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);//the recurrence relation of the problem
+            //dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);//the recurrence relation of the problem
+            int temp = Math.max(nums[i]+r1, r2);
+            r1 = r2;
+            r2 = temp;
         }
-        return dp[len-1];
+        return r2;
     }
 }
