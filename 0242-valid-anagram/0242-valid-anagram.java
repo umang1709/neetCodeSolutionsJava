52b543b7e3aef3dc89a54158df82b35e926b57cb
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-01-22 09:51:00 +0530

    Time: 2 ms (97.02%), Space: 43.3 MB (63.61%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        //first solution is converting it to charArray and sorting both strings
+        //and comparing them with .equals method, it will take O(nlogn)
+        //O(nlogn) to sort and O(1) to check with .equals method if they are equal
+        //second solution can be using int array of size 26 as given only english lowe alphabets used
+        //and then updating that array with ++ in first pass and decrementing it -- in second pass i.e.
+        //for first string and second string respectively 
+        //at last will check if int array contains only 0 then return true else false.
+        //so O(n+n+n), O(n) time will be taken.
+        int[] alpha = new int[26];
+        for(char ch:s.toCharArray())
+            alpha[ch-'a']++;
+        for(char ch:t.toCharArray())
+            alpha[ch-'a']--;
+        for(int i:alpha)
+            if(i>0)return false;
+        
+        return true;
+            
+    }
+}
