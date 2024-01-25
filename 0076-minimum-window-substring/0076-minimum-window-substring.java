0716a0bfe5cc69dc72bd414ffde29bf581a61ae4
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-01-25 11:48:52 +0530

    Time: 15 ms (54.01%), Space: 44.6 MB (50.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -8,6 +8,7 @@ class Solution {
         int matched = 0;
         int substr = 0;
         HashMap<Character, Integer> map = new HashMap<>();
+        //putting in map the string which needs to be matched with bigger string
         for(char ch:t.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
@@ -17,15 +18,29 @@ class Solution {
             //increasing the count of matched counter
             char right = s.charAt(end);
             if(map.containsKey(right)){
+                //if character in bigger string is matched with 
+                //character in map(i.e. char in smaller string)
+                //decr counter in the map and increase the matched count if that string counter
+                //came to 0
                 map.put(right,map.get(right)-1);
                 if(map.get(right) == 0)matched++;
             }
             
             while(matched == map.size()){
+                //having while loop till map size is equal to matched count
+                //as with matched count we are depicting mapSize(i.e. all characters are matched or not
+                
                 if(minLen > end - start + 1){
+                    //we are incrementing the minLen if it is greater than window size 
                     minLen = end - start + 1;
+                    //initializing the substr variable with start (i.e. start of the window)
                     substr = start;
                 }
+                
+                //this is some what confusing part as we are removing/deleting the characters here from front of window by incrementing the start 
+                //and checking if that char is concerned or not i.e. if that char is in map or not 
+                //if it is in map and count of that char == 0 then matched will be -- (as matching count will decr now after deleting that character)
+                //also increasing the counter of character in map as we need to match that character again in upcoming window as we are removing it from current
                 char deleted = s.charAt(start++);
                 if(map.containsKey(deleted)){
                     if(map.get(deleted) == 0)matched--;
@@ -33,6 +48,7 @@ class Solution {
                 }
             }
         }
+        //at last we are returning the result empty if minLen is still greater than length of string else returning substring, from substr to substr+minLen
         return minLen > s.length() ? "" : s.substring(substr, substr+minLen);
     }
 }
