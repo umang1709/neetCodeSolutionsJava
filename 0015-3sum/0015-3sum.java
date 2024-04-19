40191ae54342d66f5e42004af55f407b1d841fc2
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-04-19 11:21:02 +0530

    Time: 38 ms (44.30%), Space: 52.5 MB (20.12%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);//sorting array as to use 2 pointers.
-        //to keep in mind that triplets should not be duplicate.
-        
-        for(int i = 0;i<nums.length-2;i++){//len-2 because to keep free 2 positions for left,right pointers
-            
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                //checking above to make sure to not include duplicate 
-                //dry run with sample example and you will know.
-                int target = 0 - nums[i];
-                int left = i + 1;
-                int right = nums.length-1;
-                while(left<right){
-                    if(target == nums[left] + nums[right]){
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[left]);
-                        temp.add(nums[right]);
-                        ans.add(temp);
-                        //doing below to resolve issue of duplicacy in asnwer of triplets
-                        while(left<right && (nums[left]==nums[left+1]))left++;
-                        while(left<right && (nums[right]==nums[right-1]))right--;
-                        left++;
-                        right--;
-                    } else if(target<nums[left] + nums[right])right--;
-                      else left++;
+        Arrays.sort(nums); //sorting the input array to eradicate the duplicates
+        for(int i =0;i<nums.length-2;i++){
+            if(i!=0 && nums[i]==nums[i-1])continue;
+            int target = 0 - (nums[i]);
+            List<List<Integer>>temp = getTwoSum(nums,target,i+1);
+            if(temp.size() !=0){
+                for(List<Integer> t : temp){
+                    t.add(nums[i]);
+                    ans.add(t);
                 }
             }
-            
         }
         return ans;
+        
+        
+    }
+    private List<List<Integer>> getTwoSum(int[] arr, int target, int j){
+        int l = j;
+        int r = arr.length-1;
+        int[] nums = Arrays.copyOf(arr,arr.length);
+        List<List<Integer>> answer = new ArrayList<>();
+        while(l<r){
+            int sum = nums[l]+nums[r];
+            if(sum==target){
+                List<Integer> tem = new ArrayList<>();
+                tem.add(nums[l]);
+                tem.add(nums[r]);
+                answer.add(tem);
+                while(l<r && nums[l]==nums[l+1])l++;
+                while(l<r && nums[r]==nums[r-1])r--;
+                l++;
+                r--;
+            }else if(sum<target)l++;
+            else if(sum>target)r--;
+        }
+        return answer;
     }
 }
