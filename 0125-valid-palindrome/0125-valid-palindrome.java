8c41ec0380ff4409a35d6a191289689824fd3a89
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-04-21 11:01:30 +0530

    Time: 2 ms (98.92%), Space: 42.3 MB (97.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.isEmpty())return true;
-        //step1: build the string with only lower cases, and alphabetic characters
-        StringBuilder filtered = new StringBuilder();
-        for(char ch: s.toCharArray()){
-            if((ch>='a' && ch<='z')||(ch>='0' && ch<='9'))filtered.append(ch);
-            else if(ch>='A' && ch<='Z')filtered.append(Character.toLowerCase(ch));//here ch+'a'-'A' can be used to convert to lower case.
-        }
-        int l = 0;
-        int r = filtered.length()-1;
-        while(l<r){
-            if(filtered.charAt(l)!=filtered.charAt(r))return false;
-            else {l++;r--;}
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+            
+            if(!Character.isLetterOrDigit(start)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(end)){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(start)!=Character.toLowerCase(end)){
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
+        
     }
 }
