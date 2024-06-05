2b3723c79326ce0484f3cec4f740991d660dc550
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-06-05 23:33:54 +0530

    Time: 2 ms (89.58%), Space: 56.4 MB (11.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,28 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-     int[] ans = new int[nums.length];
-        
-        
-     //left product right product strategy is used here
-      
-     //calc. left product, from left to right and store in result
-     //to multiply it with right product to get final result
-    //imagine/code by taking simple example like {1,2,3,4}
-    
-     //step1: calc. left product
-     int left = 1;
-     for(int i=0;i<nums.length;i++){
-         ans[i] = left;
-         left *= nums[i];
-     }
-     //left product{1,1,2,6}
-     //step2: cal. right product and update final ans.
-     int right = 1;
-     for(int j=nums.length-1;j>=0;j--){
-         ans[j] *= right;
-         right *= nums[j]; //right product {24,12,4,1}
-     }
-      //final answer  {24,12,8,6}
+        //using left and right product list approach
+        int length = nums.length;
+        int[] l = new int[length];
+        int[] r = new int[length];
+        int[] ans = new int[length];
+        l[0] = 1;
+        for(int i=1;i<length;i++){
+            l[i] = nums[i-1]*l[i-1];
+        }
+        r[length-1] = 1;
+        for(int j=length-2;j>=0;j--){
+            r[j]=nums[j+1]*r[j+1];
+        }
+
+        for(int k=0;k<length;k++){
+            ans[k]=l[k]*r[k];
+        }
         return ans;
     }
 }
