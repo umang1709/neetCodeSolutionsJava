c314cb56091969f2c0d6d11e8a584a2d3aeca4e4
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-06-11 01:43:05 +0530

    Time: 26 ms (92.98%), Space: 52.3 MB (29.95%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //more simplified code::
         List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);//sorting done to apply two sum solun (using 2 pointers) later on.
-        for(int i=0;i<nums.length-2;i++){
-            if(i==0 || (i>0 && nums[i]!=nums[i-1])){//checking if prev and curr num is not equal
-            int target = 0-nums[i];
-            int l = i+1;
-            int r = nums.length-1;
-            while(l<r){
-                if(nums[l]+nums[r]==target){
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[l]);
-                    temp.add(nums[r]);
-                    ans.add(temp);
-                    while(l<r && (nums[l]==nums[l+1]))l++;//while nums are equal in l or r incrementing the indices.
-                    while(l<r && (nums[r]==nums[r-1]))r--;
-                    l++;
-                    r--;
-                } else if(nums[l]+nums[r] > target)r--;
-                  else if(nums[l]+nums[r] < target)l++;
-            }
-        } 
-            }
-        
-        return ans;
+      //-4,-1,-1,0,1,2
+      Arrays.sort(nums);
+      
+      for(int i=0;i<nums.length-2;i++){
+       if(nums[i] > 0)break;
+       else if(i>0 && nums[i]==nums[i-1])continue;
+        int target = 0 - (nums[i]);
+        int l = i+1;
+        int r = nums.length-1;
+        while(l < r){
+            if(nums[l]+nums[r] == target){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[l]); temp.add(nums[r]);
+                ans.add(temp);
+                   while(l<r && (nums[l]==nums[l+1]))l++;//while nums are equal in l or r incrementing the indices.
+                   while(l<r && (nums[r]==nums[r-1]))r--;
+
+                   l++;
+                   r--;
+            }else if(nums[l]+nums[r] > target)r--;
+            else l++;
+        }
+      }
+      return ans;
     }
 }
