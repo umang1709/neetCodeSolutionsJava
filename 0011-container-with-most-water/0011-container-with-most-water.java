b52e3a1a9db2bbd27af9fc2a728baa46d9169ba4
Umang Madeshia <122309996+umang1709@users.noreply.github.com>
2024-06-11 00:51:24 +0530

    Time: 5 ms (67.73%), Space: 57.8 MB (43.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        //brute force will take O(n^2)-- by checking each pair with two nested loops.
-        //optimal solution will take O(n) -- using two pointers.
-        int l = 0;
-        int r = height.length - 1;
-        int res = Integer.MIN_VALUE;
-        int area = 0;
-        while(l<r){
-            area = (r-l) * Math.min(height[l], height[r]);
-            res = Math.max(res,area);
-            if(height[l]<height[r])l++;
-            else r--;
+        //bruteForce solution
+        int maxArea = Integer.MIN_VALUE;
+        int currArea = 0;
+        int len = height.length;
+        int l,r;
+        l=0;
+        r = len-1;
+        while(l < r){
+            currArea = Math.min(height[l],height[r])*(r-l);
+            maxArea = Math.max(maxArea,currArea);
+            if(height[l] <= height[r]){
+                l++;
+            }else r--;
         }
-        return res;
-        
+       return maxArea;
+       
     }
 }
